cfcf2f00a3143d5fc76226afe51573652d502c7d
MPsoftwarenstuff <32528693+MPsoftwarenstuff@users.noreply.github.com>
2020-08-09 18:12:53 -0400

    Update hw6.js

--- hw6.js
+++ hw6.js
@@ -51,32 +51,35 @@ function formcollect() {
   var z = Number(document.getElementById("fourthnum").value);
   var temp = 0;
 
-  document.write("<h2>" + "You entered: " + w + ", " + x + ", " + y + ", " + z + "</h2>");
+  $("#table").append("<h3>You entered: " + w + ", " + x + ", " + y + ", " + z + "</h3>");
   //build table using bootstrap and javascript
-  document.write('<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"><center><table class="table table-dark"><thead><tr>');
-  document.write("<th scope='col'></th>");
   if (x < w) {
     temp = w;
     w = x;
     x = temp;
   }
-  for (var a = w; a <= x; a++) {
-      document.write("<th scope='col'>" + a + '</th>');
-  }
-  document.write("</tr></thead><tbody>");
   if (z < y) {
     temp = y;
     y = z;
     z = temp;
   }
-  for (a = y; a <= z; a++) {
-    document.write("<tr>");
-    document.write("<th scope='row'>" + a + "</th>");
-    for (b = w; b <= x; b++) {
-      document.write("<td>" + a*b + "</td>");
 
-    }
-    document.write("</tr>");
+  var table = '<table class="table table-dark"><thead><tr><th scope="col">0</th>';
+
+  for (var a = w; a <= x; a++) {
+      table += "<th scope='col'>" + a + "</th>";
   }
-  document.write("</table></center>");
+
+  table += "</tr></thead><tbody>";
+
+  for (var b = y; b <= z; b++) {
+    table += "<tr>";
+    table += "<th scope='row'>" + b + "</th>";
+    for (var c = w; c <= x; c++) {
+      table += "<td>" + b*c + "</td>";
+    }
+    table += "</tr>";
+  }
+  table += "</table>";
+  $("#table").append(table);
 }
